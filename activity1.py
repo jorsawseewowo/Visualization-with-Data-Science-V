b6cdf40f28c345b21ee0d5733040ebeac7d1f176
jorsawseewowo <joachimandres.seydel@gmail.com>
2026-07-04 18:39:53 +0700

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,20 +1,50 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
 
-HouseDF = pd.read_csv('USA_Housing.csv')
+df = pd.read_csv("heart.csv")
 
-corr = HouseDF.select_dtypes(include="number").corr()
+df.head()
 
-HouseDF.head()
+df.shape
 
-HouseDF.info()
+df.columns
 
-HouseDF.columns
+df.describe()
 
-sns.pairplot(HouseDF)
-plt.figure(figsize=(10,8))
+df.isnull().sum()
 
-sns.heatmap(corr, annot=True, cmap="coolwarm")
+print(df.info())
+
+df.hist(figsize=(12,12), layout=(5,3))
+
+df.plot(kind='box', subplots=True, layout=(5,3), figsize=(12, 12))
+plt.show()
+
+sns.barplot(data=df, x='sex', y='chol', hue='target', palette='spring')
+
+df['sex'].value_counts()
+
+df['target'].value_counts()
+
+df['thal'].value_counts()
+
+plt.figure(figsize=(20,10))
+sns.heatmap(df.corr(), annot=True, cmap='terrain')
+
+sns.countplot(x='sex', data=df, palette='husl', hue='target')
+
+sns.countplot(x="target", palette="BuGn", data=df)
+
+sns.countplot(x="ca", hue="target", data=df)
+
+plt.scatter(df['age'], df['chol'], c="green",
+            linewidths= 2,
+            marker="s",
+            edgecolors="green",
+            s = 50)
+
 plt.show()
